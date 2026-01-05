6a2a77374052248736ab90aeada91963e62ae57c
itz-rudr <rudrag26.2@gmail.com>
2026-01-05 23:26:17 +0530

    Program to print all permutations of string using swap-based

---
+++ Permutations_of_string_swap_based.cpp
@@ -0,0 +1,26 @@
+//Program to print all permutations of string using swap-based
+#include<stdio.h>
+int main(){
+	int n;
+	printf("Enter size:\n");
+	scanf("%d",&n);
+	char str[n+1],temp;
+	printf("Enter string:\n");
+	scanf("%s",str);
+	printf("permutations are:\n");
+	for(int i=0;i<n;i++){
+		for(int j=i;j<n;j++){
+			temp=str[i];
+			str[i]=str[j];
+			str[j]=temp;
+			for(int k=0;k<n;k++){
+				printf("%c",str[k]);
+			}
+		printf("\n");
+		temp=str[i];
+		str[i]=str[j];
+		str[j]=temp;
+		}
+	}
+	return 0;
+}
